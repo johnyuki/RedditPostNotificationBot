9eccb402afaed27d4d2d715b03160eeb05c11302
Ashley O'Connor <30492756+ashleyoconnor@users.noreply.github.com>
2017-10-20 19:00:14 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-username = ''
-password = ''
-client_id = ''
+# Guide - https://i.imgur.com/7MpgEgL.png
+
+username = '' # Enter your username without the /u/
+password = '' # Enter your password
+client_id = '' # Follow instructions on github page and the screenshot linked above for these last two.
 client_secret = ''
 user_agent = 'Notifies user of new posts in defined subreddits. Created by /u/john_yuki.'

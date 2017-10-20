82b516414dde056688bf9472107180e31bc26c38
Ashley O'Connor <30492756+ashleyoconnor@users.noreply.github.com>
2017-10-20 19:39:53 +0100

    Update Subreddit Notification Bot.py

--- Subreddit Notification Bot.py
+++ Subreddit Notification Bot.py
@@ -10,31 +10,43 @@ SUBREDDITS = 'subreddit1+subreddit2+subreddit3'
 
 
 def connect_to_reddit():
-    reddit = praw.Reddit(
-        username = config.username,
-        password = config.password,
-        client_id = config.client_id,
-        client_secret = config.client_secret,
-        user_agent = config.user_agent
-    )
-    return reddit
+    try:
+        reddit = praw.Reddit(
+            username = config.username,
+            password = config.password,
+            client_id = config.client_id,
+            client_secret = config.client_secret,
+            user_agent = config.user_agent
+        )
+        return reddit
+    except Exception as e:
+        print(e)
+        time.sleep(60)
 
 
 def notify(submission):
-    print('Found a new post in {}: {}'.format(submission.subreddit, submission.shortlink))
+    try:
+        print('Found a new post in {}: {}'.format(submission.subreddit, submission.shortlink))
 
-    # This is the name of the file that gets played when it finds a new post.
-    # You can change the file to whatever you like, as long as you update it below.
-    playsound('NotificationSound.mp3')
+        # This is the name of the file that gets played when it finds a new post.
+        # You can change the file to whatever you like, as long as you update it below.
+        playsound('NotificationSound.mp3')
+    except Exception as e:
+        print(e)
+        time.sleep(60)
     
 
 def main():
-    start_time = time.time()
-    reddit = connect_to_reddit()
-    while True:
-        for submission in reddit.subreddit(SUBREDDITS).stream.submissions():
-            if submission.created_utc > start_time:
-                notify(submission)
+    try:
+        start_time = time.time()
+        reddit = connect_to_reddit()
+        while True:
+            for submission in reddit.subreddit(SUBREDDITS).stream.submissions():
+                if submission.created_utc > start_time:
+                    notify(submission)
+    except Exception as e:
+        print(e)
+        time.sleep(60)
 
 
 if __name__ == '__main__':
